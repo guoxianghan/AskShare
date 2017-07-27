aa65792f7b42a1064044edbeafecd1604620508c
guoxianghan <1146267220@qq.com>
2017-07-27 17:52:41 +0800

    JUST INIT

---
+++ AskShare/app/views.py
@@ -0,0 +1,51 @@
+﻿#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+#写在这个位置吗 
+    list1 = ['physics', 'chemistry', 1997, 2000]
+    return render(
+        request,
+        'app/index.html',
+        {
+            'arr':list1,
+            'title':'AskShare',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
